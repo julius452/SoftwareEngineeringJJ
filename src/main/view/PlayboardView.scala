52d1a4be8c4ca758651a044c8edd4c2ca6f3a80d
Julian <bacher.juli@gmail.com>
2024-10-24 11:01:27 +0200

    changed playboard print

--- src/main/view/PlayboardView.scala
+++ src/main/view/PlayboardView.scala
@@ -7,8 +7,17 @@ object PlayboardView {
     println("Hier ist ein erster Entwurf unseres Spielfeldes:\n")
 
     // Ausgabe des Spielfelds
-    println(spielfeld.mkString(" | "))
+    printPlayBoard(spielfeld)
 
+    // Ausgabe der Häuser
+    printHouses(house1, house2, house3, house4)
+  }
+
+  def printPlayBoard(spielfeld: Array[String]): Unit = {
+    println(spielfeld.mkString(" | "))
+  }
+
+  def printHouses(house1: Array[String], house2: Array[String], house3: Array[String], house4: Array[String]): Unit = {
     println("\nHäuser der Spieler:")
     println("Player1 House: " + house1.mkString(" | "))
     println("Player2 House: " + house2.mkString(" | "))
