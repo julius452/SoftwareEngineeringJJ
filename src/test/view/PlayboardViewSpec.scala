193d496b1ff0be0a5fb9ee89eb6446ce3ffb5b1a
julius452 <164411457+julius452@users.noreply.github.com>
2024-10-24 11:05:55 +0200

    Changed project structure

---
+++ src/test/view/PlayboardViewSpec.scala
@@ -0,0 +1,8 @@
+package view
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers._
+import view.PlayboardView.*
+
+class PlayboardViewSpec extends AnyWordSpec {
+
+}
